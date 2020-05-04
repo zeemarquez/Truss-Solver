b7d906d82281569b9d0b852a63bff759f128755c
Zee Marquez <100409389@alumnos.uc3m.es>
2020-05-04 09:37:19 +0200

    commit inicial

---
+++ trussSolver01.py
@@ -0,0 +1,235 @@
+import numpy as np
+import numpy.linalg as la 
+import math as mt 
+import pygame as pg 
+
+def checkInMembers(members, relation):
+    i = relation[0]
+    j = relation[1]
+    output = False
+    for n in members:
+        if (i in n) and (j in n):
+            output = True
+    return output
+
+
+class Truss:
+    def __init__(self,nodes_, connections_):
+        self.nodes = nodes_
+        self.connections = connections_
+
+        N = len(nodes_)
+
+        self.loads = np.zeros(N*2)
+        self.bars = []
+        j = 0
+        for connection in connections_:
+            for i in connection:
+                if not checkInMembers(self.bars,[i,j]):
+                    self.bars.append([i,j])
+            j += 1
+        nForces = len(self.bars)
+        self.G = np.zeros((2*N,nForces))
+        self.F = np.zeros(len(self.bars))
+
+        for i in range(2*N):
+            if (i%2 == 0): #El indice de nodo es par por lo tanto se trata de la suma en x
+                iNode = int(i/2)
+                for iForce in range(nForces):
+                    if iNode in self.bars[iForce]:
+                        bar = list(self.bars[iForce])
+                        bar.remove(iNode)
+                        j = bar[0]
+                        self.G[i,iForce] = self.c(iNode,j)
+                    else:
+                        self.G[i,iForce] = 0
+            else : #El indice de nodo es impar por lo tanto se trata de la suma en y
+                iNode = int((i-1)/2) 
+                for iForce in range(nForces):
+                    if iNode in self.bars[iForce]:
+                        bar = list(self.bars[iForce])
+                        bar.remove(iNode)
+                        j = bar[0]
+                        self.G[i,iForce] = self.s(iNode,j)
+                    else:
+                        self.G[i,iForce] = 0
+                                           
+
+    def addLoad(self,node, load):
+        iX = node*2
+        iY = node*2 + 1
+        self.loads[iX] = -load[0]
+        self.loads[iY] = -load[1]
+    
+    def c(self, i, j):
+        x0 = self.nodes[i][0]
+        x1 = self.nodes[j][0]
+        y0 = self.nodes[i][1]
+        y1 = self.nodes[j][1]
+
+        d = mt.sqrt( (x0 - x1)**2 + (y0 - y1)**2 )
+        if d == 0:
+            return 0
+        else:
+            return (x1 - x0)/d
+
+    def s(self, i, j):
+        x0 = self.nodes[i][0]
+        x1 = self.nodes[j][0]
+        y0 = self.nodes[i][1]
+        y1 = self.nodes[j][1]
+
+        d = mt.sqrt( (x0 - x1)**2 + (y0 - y1)**2 )
+        if d == 0:
+            return 0
+        else:
+            return (y1 - y0)/d
+    
+    def k(self, i, j):
+        if j in self.connections[i]:
+            return 1
+        else:
+            return 0
+
+    def solve(self):
+
+        lst = la.lstsq(self.G, self.loads , rcond=None)
+        fmax = 0
+        for force in lst[0]:
+            if abs(force) > fmax:
+                fmax = abs(force)
+
+        self.result = lst[0]
+        self.fmax = fmax
+        
+
+class PgTruss:
+    def __init__(self,truss_,screenSize_):
+        self.truss = truss_
+        self.xmaxS = screenSize_
+        self.margin = 100
+        xmaxR = 0
+        ymaxR = 0
+        for nodei in truss_.nodes:
+            for nodej in truss_.nodes:
+                if abs(nodei[0] - nodej[0]) > xmaxR:
+                    xmaxR = abs(nodei[0] - nodej[0])
+                if abs(nodei[1] - nodej[1]) > ymaxR:
+                    ymaxR = abs(nodei[1] - nodej[1])
+
+        self.xmaxR = xmaxR
+        self.ymaxR = ymaxR 
+        self.scale =(self.xmaxS - 2*self.margin)/xmaxR
+        self.ymaxS = int((self.scale * self.ymaxR) + self.margin*2)
+        pg.init()
+        self.screen = pg.display.set_mode((self.xmaxS,self.ymaxS))
+
+    def drawNodes(self):
+        
+        black = (60,60,60)
+        white = (255,255,255)
+        red = (255, 147, 117)
+        blue = (149, 206, 252)
+        green = (87, 255, 126)
+        pg.font.init()
+        myfont = pg.font.SysFont('Lucida Sans', 10)
+        self.screen.fill(black)
+        screenNodes = []
+        nodeIndx = 0
+        for node in self.truss.nodes:
+            nodex = int((node[0] * self.scale) + self.margin)
+            nodey = int(self.ymaxS -((node[1] * self.scale) + self.margin))
+            screenNodes.append([nodex,nodey])
+            pg.draw.circle(self.screen,white,(nodex,nodey),4, 1)
+            
+            xloadI = 2*nodeIndx
+            yloadI = xloadI + 1
+            if self.truss.loads[xloadI] != 0 or self.truss.loads[yloadI] != 0 :
+                loadx = 0 - self.truss.loads[xloadI]
+                loady = 0 - self.truss.loads[yloadI]
+                text = "(" + str(loadx) + ", " + str(loady) + ")"
+                loadText = myfont.render(text, True, green)
+                self.screen.blit(loadText,(nodex - 30, nodey + 25))
+            
+            textsurface = myfont.render(str(nodeIndx), True, white)
+            self.screen.blit(textsurface,(nodex + 10, nodey + 5))
+
+            nodeIndx += 1
+            
+            
+        self.screenPoints = screenNodes
+
+        barIndx = 0
+        for bar in self.truss.bars:
+            pointA = self.screenPoints[bar[0]]
+            pointB = self.screenPoints[bar[1]]
+            midPointx = int((pointA[0] + pointB[0])/2)
+            midPointy = int((pointA[1] + pointB[1])/2)
+
+            forceBar = round(self.truss.result[barIndx],1)
+            if forceBar>0:
+                color = red
+            else:
+                color = blue
+
+            pg.draw.line(self.screen,white,pointA, pointB, 1)
+
+            textsurface = myfont.render(str(forceBar), True, color)
+            self.screen.blit(textsurface,(midPointx, midPointy - 20))
+            barIndx += 1
+        
+        
+        
+        
+        pg.display.flip()
+        run = True
+        while run:
+            event = pg.event.wait()
+            if event.type == pg.QUIT:
+                run = False  
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                run = False
+        
+
+nodes = np.array([
+    [0,0],
+    [4,0],
+    [0,4],
+    [4,4],
+    [0,8],
+    [4,8],
+    [4,12],
+    [8,12],
+    [8,8],
+    [12,8]
+    ])
+
+connections = np.array([
+    [1,2,3],
+    [0,3],          #0
+    [0,3,4],        #1
+    [0,1,2,4,5],    #2
+    [2,3,5,6],      #3
+    [3,4,6,7,8],    #4
+    [4,5,7],        #5
+    [6,5,8,9],      #6
+    [5,7,9],        #7
+    [7,8]           #8
+    ])
+
+t1 = Truss(nodes,connections)
+
+t1.addLoad(0,[-60,-280])
+t1.addLoad(1,[0,360])
+t1.addLoad(9,[60,-80])
+
+t1.solve()
+
+output = t1.result
+
+print(t1.loads)
+
+
+
+trussdraw = PgTruss(t1,800)
+trussdraw.drawNodes()
